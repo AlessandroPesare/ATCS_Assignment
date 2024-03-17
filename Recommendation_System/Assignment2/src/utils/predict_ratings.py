e880906a9cc2640f8d84f1243623db59a8913d2a
AlessandroPesare <ale.pesare@stud.uniroma3.it>
2024-03-17 18:15:46 +0100

    aggregation methods refactor

--- Recommendation_System/Assignment2/src/utils/predict_ratings.py
+++ Recommendation_System/Assignment2/src/utils/predict_ratings.py
@@ -3,44 +3,46 @@ Prediction function
 '''
 import numpy as np
 import pandas as pd
+
 # Version 1
 # most_similar = 0 --> consider all users
 # most_similar = 1 --> consider only the most similar users
 
-def predict_rating(user_id, item_id, user_item_matrix,user_similarity,most_similar):
-    # Calcola la media dei rating dell'utente a
+def predict_rating(user_id, item_id, user_item_matrix, user_similarity, most_similar):
+    # Calculate the mean rating of user a
     mean_rating_a = user_item_matrix.loc[user_id].mean()
 
-    # Inizializza il numeratore e il denominatore della formula
+    # Initialize the numerator and denominator of the formula
     numerator = 0
     denominator = 0
     dict_intern = user_similarity[user_id]
 
-    if(most_similar == 1):
-        #sort values
+    if most_similar == 1:
+        # Sort values
         dict_intern = {k: v for k, v in sorted(dict_intern.items(), key=lambda item: item[1], reverse=True)}
         users = list(dict_intern.keys())
         users = users[:50]
     else:
         users = dict_intern.keys()
-    # Itera su tutti gli utenti
+    
+    # Iterate over all users
     for other_user_id in users:
-        # Verifica se l'utente b ha effettivamente valutato l'item
+        # Check if user b has actually rated the item
         if not pd.isnull(user_item_matrix.loc[other_user_id, item_id]):
-            # Calcola la media dei rating dell'utente b
+            # Calculate the mean rating of user b
             mean_rating_b = user_item_matrix.loc[other_user_id].mean()
-            # Calcola il rating di b per l'item p
+            # Calculate the rating of b for item p
             rating_b_p = user_item_matrix.loc[other_user_id, item_id]
-            # Aggiorna il numeratore e il denominatore
+            # Update the numerator and denominator
             similarity = dict_intern[other_user_id]
             numerator += similarity * (rating_b_p - mean_rating_b)
             denominator += similarity
-    # Calcola il rating predetto
+    
+    # Calculate the predicted rating
     if denominator != 0:
         predicted_rating = mean_rating_a + (numerator / denominator)
     else:
-        predicted_rating = mean_rating_a  # Evita la divisione per zero
-
+        predicted_rating = mean_rating_a  # Avoid division by zero
 
     return predicted_rating
 
@@ -48,40 +50,40 @@ def predict_rating(user_id, item_id, user_item_matrix,user_similarity,most_simil
 # most_similar = 0 --> consider all users
 # most_similar = 1 --> consider only the most similar users
 
-def predict_rating_with_abs(user_id, item_id, user_item_matrix,user_similarity,most_similar):
-    # Calcola la media dei rating dell'utente a
+def predict_rating_with_abs(user_id, item_id, user_item_matrix, user_similarity, most_similar):
+    # Calculate the mean rating of user a
     mean_rating_a = user_item_matrix.loc[user_id].mean()
 
-    # Inizializza il numeratore e il denominatore della formula
+    # Initialize the numerator and denominator of the formula
     numerator = 0
     denominator = 0
     dict_intern = user_similarity[user_id]
 
-    if(most_similar == 1):
-        #sort for value
+    if most_similar == 1:
+        # Sort for value
         dict_intern = {k: v for k, v in sorted(dict_intern.items(), key=lambda item: item[1], reverse=True)}
         users = list(dict_intern.keys())
         users = users[:50]
     else:
         users = dict_intern.keys()
-    # Itera su tutti gli utenti
+    
+    # Iterate over all users
     for other_user_id in users:
-        # Verifica se l'utente b ha effettivamente valutato l'item
+        # Check if user b has actually rated the item
         if not pd.isnull(user_item_matrix.loc[other_user_id, item_id]):
-            # Calcola la media dei rating dell'utente b
+            # Calculate the mean rating of user b
             mean_rating_b = user_item_matrix.loc[other_user_id].mean()
-            # Calcola il rating di b per l'item p
+            # Calculate the rating of b for item p
             rating_b_p = user_item_matrix.loc[other_user_id, item_id]
-            # Aggiorna il numeratore e il denominatore
+            # Update the numerator and denominator
             similarity = dict_intern[other_user_id]
             numerator += similarity * (rating_b_p - mean_rating_b)
             denominator += abs(similarity)
             
-    # Calcola il rating predetto
+    # Calculate the predicted rating
     if denominator != 0:
         predicted_rating = mean_rating_a + (numerator / denominator)
     else:
-        predicted_rating = mean_rating_a  # Evita la divisione per zero
-
+        predicted_rating = mean_rating_a  # Avoid division by zero
 
     return predicted_rating
